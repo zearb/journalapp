421e6a529f00bf49c3934b7f09989e5e7ea6d743
Aldo <aldora.ez@gmail.com>
2020-10-16 06:46:42 -0500

    build docs 2

---
+++ docs/precache-manifest.f55b726ab5c18b9333696a4814f44002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f65fe7bc75dfb82eacb56f750048286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f05ee2f59c8f34c12c1c",
+    "url": "/static/css/main.66ff6cab.chunk.css"
+  },
+  {
+    "revision": "828b811474aae4a61f6e",
+    "url": "/static/js/2.5118cad1.chunk.js"
+  },
+  {
+    "revision": "dad1e10c751afc1e6fffeb8be63f9aab",
+    "url": "/static/js/2.5118cad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05ee2f59c8f34c12c1c",
+    "url": "/static/js/main.8ed1d513.chunk.js"
+  },
+  {
+    "revision": "409e9927f6ba056af560",
+    "url": "/static/js/runtime-main.5e8c77a5.js"
+  }
+]);
